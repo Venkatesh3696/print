8bf9f963755d38b2688a09000d3450fc9b513b21
Venkatesh3696 <venkateshpentakota888@gmail.com>
2025-04-30 13:01:36 +0530

    add cors

--- server/config/corsConfig.js
+++ server/config/corsConfig.js
@@ -1,4 +1,21 @@
+// export const corsOptions = {
+//   origin: ["http://localhost:5173", "https://mycommerce-1.onrender.com"],
+//   credentials: true,
+// };
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://print-1-h34c.onrender.com",
+];
+
 export const corsOptions = {
-  origin: ["http://localhost:5173", "https://print-1-h34c.onrender.com/"],
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 };
